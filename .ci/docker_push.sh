f2bafcc9cf5a5048aed1a9ab39e6e6a106c44445
bkellerman <bert@reflexer.finance>
2020-12-08 20:27:56 +0000

    travis docker tag with commit

--- .ci/docker_push.sh
+++ .ci/docker_push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker build -t reflexer/settlement-keeper .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag reflexer/settlement-keeper reflexer/settlement-keeper:$COMMIT
 docker push reflexer/settlement-keeper
+docker push reflexer/settlement-keeper:$COMMIT
