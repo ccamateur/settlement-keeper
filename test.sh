1889ecd0aef682c68e5977bdb08f2298bc6258ab
bkellerman <bert@reflexer.finance>
2020-12-08 20:09:00 +0000

    support for testing multiple testchains. use prai-demo pyflex release

--- test.sh
+++ test.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 
-CONFIG="testchain-value-fixed-discount-uniswap-multisig-basic"
-while getopts :c: option
+declare -a TESTCHAINS=("rai-testchain-value-fixed-discount-uniswap-multisig-basic" "rai-testchain-value-fixed-discount-uniswap-multisig-safe")
+
+declare -a TESTCHAINS=("testchain-value-fixed-discount-uniswap-vote-quorum")
+
+run_test () {
+  export TESTCHAIN=$1
+
+  # Pull the docker image
+  docker pull reflexer/${TESTCHAIN}
+
+  pushd ./lib/pyflex
+
+  # Remove existing container if tests not gracefully stopped
+  docker-compose -f config/${TESTCHAIN}.yml down
+
+
+  # Start parity and wait to initialize
+  echo Starting parity
+  docker-compose -f config/${TESTCHAIN}.yml up -d parity
+  sleep 2
+  popd
+
+  export PYTHONPATH=$PYTHONPATH:./lib/pyflex:./lib/auction-keeper:./lib/pygasprice-client
+  py.test -s --cov=src --cov-report=term --cov-append tests/test_settlement_keeper.py
+  TEST_RESULT=$?
+
+  echo Stopping container
+  pushd ./lib/pyflex
+  docker-compose -f config/${TESTCHAIN}.yml down
+  popd
+
+  return $TEST_RESULT
+
+}
+
+
+# If passing a single config or test file, just run tests on one testchain
+while getopts :c:f: option
 do
 case "${option}"
 in
-c) CONFIG=${OPTARG};;
+c) TESTCHAIN=${OPTARG};;
 esac
 done
 
-# Pull the docker image
-docker pull reflexer/testchain-pyflex:${CONFIG}
-pushd ./lib/pyflex
-# Stop any existing containers
-docker-compose -f config/${CONFIG}.yml down
+if [ ! -z ${TESTCHAIN} ];then
+  echo "Testing on testchain ${TESTCHAIN}"
+  run_test $TESTCHAIN
+  exit $?
+fi
 
-# Start the docker image and wait for parity to initialize
-docker-compose -f config/${CONFIG}.yml up -d
-sleep 2
-popd
+COMBINED_RESULT=0
+for config in "${TESTCHAINS[@]}"
+do
+  run_test $config
+  COMBINED_RESULT=$(($COMBINED_RESULT + $?))
+done
 
-PYTHONPATH=$PYTHONPATH:./lib/pyflex:./lib/auction-keeper:./lib/pygasprice-client py.test -s \
---cov=src --cov-report=term --cov-append tests/test_settlement_keeper.py $@
-TEST_RESULT=$?
 
-echo Stopping container
-pushd ./lib/pyflex
-docker-compose -f config/${CONFIG}.yml down
-popd
-
-exit $TEST_RESULT
+exit $COMBINED_RESULT
